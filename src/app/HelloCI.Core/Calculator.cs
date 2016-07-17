b13ec94da366b998b81dd2f231b5ec1fd9375ec6
babaksoft <babak.eslamieh@gmail.com>
2016-07-17 17:30:51 +0430

    Added Sandcastle help project file and a new class (with test code) to test API documentation generation.

---
+++ src/app/HelloCI.Core/Calculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCI.Core
+{
+    /// <summary>
+    /// Implements a redundant calculator.
+    /// </summary>
+    public class Calculator
+    {
+        /// <summary>
+        /// Adds two numbers.
+        /// </summary>
+        /// <param name="left">Left number</param>
+        /// <param name="right">Right number</param>
+        /// <returns></returns>
+        public double Add(double left, double right)
+        {
+            return left + right;
+        }
+
+        /// <summary>
+        /// Subtracts two numbers.
+        /// </summary>
+        /// <param name="left">Left number</param>
+        /// <param name="right">Right number</param>
+        /// <returns></returns>
+        public double Subtract(double left, double right)
+        {
+            return left - right;
+        }
+
+        /// <summary>
+        /// Multiplies two numbers.
+        /// </summary>
+        /// <param name="left">Left number</param>
+        /// <param name="right">Right number</param>
+        /// <returns></returns>
+        public double Multiply(double left, double right)
+        {
+            return left * right;
+        }
+    }
+}
---
+++ src/test/HelloCI.Tests/CalculatorTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelloCI.Core;
+using NUnit.Framework;
+
+namespace HelloCI.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void ShouldAddTwoNumbers()
+        {
+            // Arrange
+            var calc = new Calculator();
+
+            // Act
+            var sum = calc.Add(12, 34);
+
+            // Assert
+            Assert.That(sum, Is.EqualTo(46));
+        }
+
+        [Test]
+        public void ShouldSubtractTwoNumbers()
+        {
+            // Arrange
+            var calc = new Calculator();
+
+            // Act
+            var result = calc.Subtract(12, 34);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(-22));
+        }
+
+        [Test]
+        public void ShouldMultiplyTwoNumbers()
+        {
+            // Arrange
+            var calc = new Calculator();
+
+            // Act
+            var result = calc.Multiply(12, 34);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(408));
+        }
+    }
+}
